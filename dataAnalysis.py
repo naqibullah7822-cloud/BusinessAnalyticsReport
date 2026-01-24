aa08929fc1119ab7a40d2a08190eb8a3da1cc09d
Naqib Khan <naqibullah.7822@gmail.com>
2026-01-25 00:29:34 +0500

    dataAnalysis

---
+++ dataAnalysis.py
@@ -0,0 +1,19 @@
+# %% [markdown]
+# Notebook to do the data analysis
+
+# %% [markdown]
+# import libraries
+
+# %%
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from sklearn.datasets import load_iris
+
+df = sns.load_dataset('tips')
+print(df)
+
+
+
+
